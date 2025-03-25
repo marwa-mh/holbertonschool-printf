cc6d02405df60425b0d60b0f58db8c8c55f749aa
Josephine Dang <dangjos02@gmail.com>
2025-03-25 05:43:28 +0000

    fix betty issues

--- _printf.c
+++ _printf.c
@@ -12,60 +12,61 @@
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    int i, j;
-    char *str;
+	va_list args;
+	int count = 0;
+	int i, j;
+	char *str;
+	char c;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == '\0')
-            {
-                return (-1);
-            }
-            if (format[i + 1] == 'c')
-            {
-                char c = va_arg(args, int);
-                _putchar(c);
-                count++;
-                i++;
-            }
-            else if (format[i + 1] == 's')
-            {
-                str = va_arg(args, char *);
-                if (!str)
-                    str = "(null)";
-                for (j = 0; str[j] != '\0'; j++)
-                {
-                    _putchar(str[j]);
-                    count++;
-                }
-                i++;
-            }
-            else if (format[i + 1] == '%')
-            {
-                _putchar('%');
-                count++;
-                i++;
-            }
-            else
-            {
-                _putchar('%');
-                _putchar(format[(i + 1)]);
-                count += 2;
-                i++;
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-            count++;
-        }
-    }
-    va_end(args);
-    return (count);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{
+				return (-1);
+			}
+			if (format[i + 1] == 'c')
+			{
+				c = va_arg(args, int);
+				_putchar(c);
+				count++;
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(null)";
+				for (j = 0; str[j] != '\0'; j++)
+				{
+					_putchar(str[j]);
+					count++;
+				}
+				i++;
+			}
+			else if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				count++;
+				i++;
+			}
+			else
+			{
+				_putchar('%');
+				_putchar(format[(i + 1)]);
+				count += 2;
+				i++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
+	va_end(args);
+	return (count);
 }
