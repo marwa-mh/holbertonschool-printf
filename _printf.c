a368bb574845a3e75710826fad423fbe8fc3572d
fvdumaa <fadumaaah@gmail.com>
2025-03-25 15:35:43 +1100

    test null case

--- _printf.c
+++ _printf.c
@@ -21,8 +21,12 @@ int _printf(const char *format, ...)
 
     for (i = 0; format[i] != '\0'; i++)
     {
-        if (format[i] == '%' && format[i + 1] != '\0')
+        if (format[i] == '%')
         {
+            if (format[i + 1] = '\0')
+            {
+                return (count);
+            }
             if (format[i + 1] == 'c')
             {
                 char c = va_arg(args, int);
