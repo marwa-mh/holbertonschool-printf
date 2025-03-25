f659e30a673fac43394417430fad2d9d42a56c63
Marwa <marwa.almahmoud@hotmail.com>
2025-03-25 20:29:29 +0000

    reduce the number of lines for betty

--- _printf.c
+++ _printf.c
@@ -1,33 +1,24 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
-
 /**
- * printf - Custom printf that produces output according to format
- *
+ * _printf - Custom printf that produces output according to format
  * @format: arguments to printf
- *
  * Return: Count of argument
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
-	int i, j;
-	char *str;
-	char c;
+	int count = 0, i, j;
+	char *str, c;
 
 	va_start(args, format);
-
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
-			{
 				return (-1);
-			}
 			if (format[i + 1] == 'c')
 			{
 				c = va_arg(args, int);
