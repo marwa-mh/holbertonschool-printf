792b5fd8f55e6f135488f92b74c6ffb355168baa
fvdumaa <fadumaaah@gmail.com>
2025-03-25 15:39:25 +1100

    test

--- _printf.c
+++ _printf.c
@@ -48,7 +48,7 @@ int _printf(const char *format, ...)
             }
             else if (format[i + 1] == '%')
             {
-                _putchar(format[i]);
+                _putchar('%');
                 count++;
                 i++;
             }
