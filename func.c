a816c8120600398c7fe5972ca3f841f0b46801f8
Marwa <marwa.almahmoud@hotmail.com>
2025-03-26 02:36:20 +0000

    Add description to the function for betty

--- func.c
+++ func.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_char - print character
+ * @args: args
+ * Return: int
+ */
 int print_char(va_list args)
 {
 	char c = va_arg(args, int);
@@ -7,12 +12,22 @@ int print_char(va_list args)
 	_putchar(c);
 	return (1);
 }
+/**
+ * print_signed_integer - print signed integer
+ * @args: args
+ * Return: int
+ */
 int print_signed_integer(va_list args)
 {
 	int c = va_arg(args, int);
 
 	return (print_number(c));
 }
+/**
+ * print_int - print int
+ * @args: args
+ * Return: int
+ */
 int print_int(va_list args)
 {
 	int len = 0;
@@ -20,6 +35,11 @@ int print_int(va_list args)
 	len = 1 + print_number(va_arg(args, int));
 	return (len);
 }
+/**
+ * print_number - print number
+ * @n: the number
+ * Return: int
+ */
 int print_number(int n)
 {
 	int len = 0;
@@ -39,6 +59,11 @@ int print_number(int n)
 		_putchar(n + '0');
 	return (len + 1);
 }
+/**
+ * print_string - print string
+ * @args: args
+ * Return: int
+ */
 int print_string(va_list args)
 {
 	char *str = va_arg(args, char *);
